3e62d2a09029778ce1d08fd129900307b082b38c
leo_v <leonardo.vic3nte@gmail.com>
2024-12-15 12:00:24 -0300

    make brand component link to home

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import clothesService from "@/services/clothesApiService.ts";
 import { ClothCategory } from "@/types/ClothItem.ts";
+import Header from "@/components/Header";
 
 function App() {
 	const menClothesQuery = useQuery({
@@ -14,7 +15,11 @@ function App() {
 		queryFn: clothesService.getWomenClothes,
 	});
 
-	return <div className="bg-background-cream min-h-screen">ayy bro</div>;
+	return (
+		<div className="bg-background-cream min-h-screen px-12 py-4">
+			<Header />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+// @todo Move the brand component here, create the navbar using shadcn menubar and finally the cart, and search
+
+import Brand from "@/components/Header/Brand.tsx";
+
+function Header() {
+	return <Brand withLogoImg={true} />;
+}
+
+export default Header;
