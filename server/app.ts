6467e9d3d431358ff2bc299d7ba9811f7fb672d6
leo_v <leonardo.vic3nte@gmail.com>
2024-12-17 22:47:11 -0300

    finish most of the server

---
+++ server/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import "dotenv/config";
+import helmet from "helmet";
+import compression from "compression";
+import shoesRouter from "./router/shoesRouter.js";
+
+const app = express();
+
+
+app.use(helmet());
+app.use("/assets", express.static(path.join(import.meta.dirname, "public")));
+app.use(compression());
+
+app.use("/shoes", shoesRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+});
+
+
+
---
+++ server/router/shoesRouter.ts
@@ -0,0 +1,22 @@
+import { Router, Request, Response } from "express";
+import shoesData from "../data.js";
+
+const shoesRouter = Router();
+
+shoesRouter.get("/", (_req: Request, res: Response) => {
+    res.json(shoesData);
+});
+
+shoesRouter.get("/:id", (req: Request, res: Response) => {
+    const { id } = req.params;
+    const foundShoe = shoesData.find(shoe => shoe.id === Number(id));
+
+    if (!foundShoe) {
+        res.sendStatus(404);
+    }
+
+    res.json(foundShoe);
+});
+
+
+export default shoesRouter;
